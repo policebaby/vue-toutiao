e195f2060ff7df283ab6d80ad5f70b8375801438
Beyond <policebaby111@gmail.com>
2023-08-05 16:10:14 +0630

    测试第一次推送

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <keep-alive :include="cachePages" >
-       <router-view />
+    <keep-alive :include="cachePages">
+      <router-view />
     </keep-alive>
-     
   </div>
 </template>
 
 <script>
-  import './styles/index.css';
-  import {mapState} from 'vuex';
-  export default {
-    name: 'App',
-    components: {
-
-    },
-    computed:{
-      ...mapState(['cachePages'])
-    }
-  }
+import "./styles/index.css";
+import { mapState } from "vuex";
+export default {
+  name: "App",
+  components: {},
+  computed: {
+    ...mapState(["cachePages"]),
+  },
+};
 </script>
 
 <style>
-.test{
-  color: green;
+.testfirstpush {
+  color: gray;
 }
-
 </style>
